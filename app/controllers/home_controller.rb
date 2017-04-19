3cfb9230e49c18377322102fe7cfe9bda88d3d8c
MAC <MAC@MACdeiMac-2.local>
2017-04-19 17:35:13 +0800

    update footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def index
-
+  	@select_menu = "home"
+  	pp "-----#{request}------"
   end
 
   def contact
-
+  	@select_menu = "contact"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
 
-  scope "(:locale)", locale: /en|zh_CN/ do
+   scope "(:locale)", locale: /en|zh_CN/ do
     root 'home#index'
     resources :home do
       collection do
-        get :contact
+        get 'contact'
       end
     end
-  end
+   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
